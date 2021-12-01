a3fa37e92cc1dfe9227487f0004a0d8481aa6137
Seiji Spieker <seiji.spieker@gmail.com>
2021-12-01 11:18:30 +0100

    Download and parse all events with MR market from toto.

--- smartbet.py
+++ smartbet.py
@@ -2,6 +2,7 @@ import json
 import os
 
 import requests
+import unidecode
 
 
 def get_request(url, params=None, headers=None):
@@ -13,11 +14,67 @@ def get_request(url, params=None, headers=None):
     return response
 
 
+def normalize(string):
+    return unidecode.unidecode(string.strip().lower())
+
+
+def toto(events):
+    print('Downloading events from toto...')
+
+    url = 'https://content.toto.nl/content-service/api/v1/q/time-band-event-list'
+    params = {
+        'drilldownTagIds': 2, # Test with 11.
+        'maxTotalItems': 100000000,
+        'maxEventsPerCompetition': 100000000,
+        'maxCompetitionsPerSportPerBand': 100000000,
+        'maxEventsForNextToGo': 100000000,
+        'includeChildMarkets': 'true',
+        'marketSortsIncluded': 'MR'
+    }
+
+    response = get_request(url, params=params)
+
+    for time_band_event in response.json()['data']['timeBandEvents']:
+        for event in time_band_event['events']:
+            home = normalize(event['teams'][0]['name'])
+            away = normalize(event['teams'][1]['name'])
+            time = event['startTime']
+            id = f'{home} v {away} - {time}'
+            events[id] = {}
+            events[id]['home'] = home
+            events[id]['away'] = away
+            events[id]['sport'] = normalize(event['category']['name'])
+            events[id]['region'] = normalize(event['class']['name'])
+            events[id]['division'] = normalize(event['type']['name'])
+            market = event['markets'][0]
+            market_label = market['subType']
+            events[id]['markets'] = {market_label: {}}
+
+            for outcome in market['outcomes']:
+                outcome_label = outcome['subType']
+
+                if outcome_label == 'H':
+                    outcome_label = '1'
+                if outcome_label == 'D':
+                    outcome_label = 'X'
+                if outcome_label == 'A':
+                    outcome_label = '2'
+
+                events[id]['markets'][market_label][outcome_label] = {
+                    'odds' : outcome['prices'][0]['decimal'],
+                    'bookmaker' : 'Toto'
+                }
+
+    print(f'Downloaded {len(events)} events from toto.')
+
+
 def main():
     events = {}
 
     if not os.path.exists('output'):
         os.mkdir('output')
+
+    toto(events)
 
     with open('output/smartbet.json', 'w') as f:
         json.dump(events, f)
