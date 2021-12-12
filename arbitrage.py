0de55c239d2ddccd81fba84259e1d496510a232e
Seiji Spieker <seiji.spieker@gmail.com>
2021-12-12 09:47:09 +0100

    Calculate roi for arbitrage bets.

--- arbitrage.py
+++ arbitrage.py
@@ -6,15 +6,15 @@ def arbitrage(events):
 
     for id, event in events.items():
         for market in event['markets'].values():
-            inverse = 0
+            arbitrage = 0
 
             for outcome in market.values():
-                inverse += 1 / outcome['odds']
+                arbitrage += 1 / outcome['odds']
 
-            if 0 < inverse < 1:
+            if 0 < arbitrage < 1:
+                event['roi (%)'] = ((1 / arbitrage) - 1) * 100
                 arbitrageEvents[id] = event
                 count += 1
 
     print(f'Number of arbitrage bets: {count}')
-
     return arbitrageEvents
